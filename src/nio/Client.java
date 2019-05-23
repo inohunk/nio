12e319ffef9462e638d8b2b3d1330cd70eb2cccb
hunkel <kolan19988@yandex.ru>
2019-05-23 12:18:47 +0300

    added feature connect to host

--- src/nio/Client.java
+++ src/nio/Client.java
@@ -1,12 +1,16 @@
 package nio;
 
+import logging.Log;
+
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 
-public class Client {
+public class Client implements Runnable {
 
+    private static final Log log = Log.getInstance();
     private static final String DEFAULT_HOST = "localhost";
     private static final int DEFAULT_PORT = 2023;
     private static final int DEFAULT_TIMEOUT = 3000;
@@ -15,9 +19,12 @@ public class Client {
 
     private String host = DEFAULT_HOST;
     private int port = DEFAULT_PORT;
-
+    private InetSocketAddress serverAddress;
     private SocketChannel channel;
     private Selector selector;
+    private boolean isReady = false;
+    private boolean isConnected = false;
+
 
     public Client(){}
 
@@ -40,5 +47,53 @@ public class Client {
             e.printStackTrace();
         }
         return available;
+    }
+    private boolean init(){
+        try {
+            serverAddress = new InetSocketAddress(host,port);
+            selector = Selector.open();
+            channel = SocketChannel.open();
+            channel.configureBlocking(false);
+
+            int ops = channel.validOps();
+            channel.register(selector,ops);
+            log.logi("configure finished");
+            return true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    private void connect(){
+        try {
+            channel.connect(serverAddress);
+            while (channel.isConnectionPending()){
+                channel.finishConnect();
+            }
+            log.logi("connected to "+channel.getRemoteAddress());
+            setConnected(true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            setConnected(false);
+        }
+    }
+
+    @Override
+    public void run() {
+        isReady = init();
+        if (isReady){
+            connect();
+        }
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+
+    public void setConnected(boolean connected) {
+        isConnected = connected;
     }
 }
