f1afd60c17084449d14b89a4d152b3a2742d7b39
hunkel <kolan19988@yandex.ru>
2019-05-23 10:44:58 +0300

    created basic variables

--- src/nio/Server.java
+++ src/nio/Server.java
@@ -1,4 +1,30 @@
 package nio;
 
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
 public class Server {
+
+    /*
+        VARIABLES
+     */
+    private static final int DEFAULT_PORT = 2023;
+
+    private static final int SEND_BUFFER_SIZE = 128;
+    private static final int RCV_BUFFER_SIZE = 1024;
+
+    private int port = DEFAULT_PORT;
+
+    private ServerSocketChannel channel;
+    private Selector selector;
+
+    public Server(){}
+
+    public Server(int port){
+
+    }
+
+
+
+
 }
