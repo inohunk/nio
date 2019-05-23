b5c80bce50eb62f9072039b2994c1d1403198189
hunkel <kolan19988@yandex.ru>
2019-05-23 14:13:26 +0300

    added feature selector

--- src/nio/Server.java
+++ src/nio/Server.java
@@ -5,8 +5,11 @@ import logging.Log;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+
 
 public class Server implements Runnable {
 
@@ -33,6 +36,9 @@ public class Server implements Runnable {
         this.port = port;
     }
 
+    /*
+        METHODS
+     */
     private boolean init(){
         try {
             socketAddress = new InetSocketAddress(host,port);
@@ -40,7 +46,7 @@ public class Server implements Runnable {
             socketChannel = ServerSocketChannel.open();
             socketChannel.configureBlocking(false);
             socketChannel.bind(socketAddress);
-
+            socketChannel.socket().setReceiveBufferSize(RCV_BUFFER_SIZE);
             int ops = socketChannel.validOps();
             socketChannel.register(selector,ops);
 
@@ -57,16 +63,92 @@ public class Server implements Runnable {
         try {
             while (true) {
                 int nConnections = selector.selectNow();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
                 if(nConnections == 0){
                     Thread.sleep(3000);
                     log.logi("server sleep 3secs but no connections");
                     continue;
                 }
 
+                selectionProcess(selectionKeys);
             }
         } catch(IOException | InterruptedException e){
             e.printStackTrace();
         }
+    }
+
+    private void selectionProcess(Set<SelectionKey> keys){
+        Iterator<SelectionKey> iterator = keys.iterator();
+        SelectionKey key;
+
+        while (iterator.hasNext()){
+            key = iterator.next();
+            iterator.remove();
+
+            if (key.isValid()){
+                if (key.isAcceptable()) accept(key);
+                if (key.isWritable()) write(key);
+                if (key.isReadable()) read(key);
+            }
+            keys.clear();
+        }
+    }
+
+    private void accept(SelectionKey key) {
+        try {
+            ServerSocketChannel serverSocket = (ServerSocketChannel) key.channel();
+            SocketChannel client = serverSocket.accept();
+            client.configureBlocking(false);
+            log.logi("client "+client.getRemoteAddress()+" connected");
+
+            client.register(selector,SelectionKey.OP_WRITE);
+            //TODO ADD MESSAGES QUEUE
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void read(SelectionKey key) {
+        ReadableByteChannel channelIn = (ReadableByteChannel) key.channel();
+
+        ByteBuffer bufferIn = ByteBuffer.allocate(RCV_BUFFER_SIZE);
+
+        try {
+            int bytesRead = channelIn.read(bufferIn);
+            if(bytesRead > 0){
+                log.logi("read "+bytesRead);
+            }else {
+                log.logi("no bytes read");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        key.interestOps(SelectionKey.OP_WRITE);
+
+    }
+
+    private void write(SelectionKey key) {
+        WritableByteChannel channelIn = (WritableByteChannel) key.channel();
+
+        ByteBuffer bufferOut = ByteBuffer.allocate(RCV_BUFFER_SIZE);
+
+        try {
+            int bytesWrite = channelIn.write(bufferOut);
+            if(bytesWrite > 0){
+                log.logi("write "+bytesWrite);
+            }else {
+                log.logi("no bytes write");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        key.interestOps(SelectionKey.OP_READ);
     }
 
     @Override
